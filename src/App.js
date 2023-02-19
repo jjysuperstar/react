0e009399b8b28690b9d320aaab99f730a4f068f5
jangjae <jjysuperstar@nate.com>
2023-02-20 02:06:10 +0900

    5gang

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className='gray-background'>
-        <img src={logo} lat='logo'/>
-        <h2>12344</h2>
+const customer = [
+  {
+    'id': 1,
+    'image': 'http://placeimg.com/64/64/any',
+    'name': '장재영',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'http://placeimg.com/64/64/any',
+    'name': '오랜징',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 3,
+    'image': 'http://placeimg.com/64/64/any',
+    'name': '김혁',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  }
+]
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        {
+          customer.map( c => {
+            return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+            );
+          })
+        }
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
